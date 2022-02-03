212878f93fb9a786d7f027667fd42acd700e1224
joshuasimon-taulia <joshua.simon@taulia.com>
2022-02-02 20:16:00 -0800

    fix: allow Github App to clone forks of a private repository

--- server/events/github_app_working_dir.go
+++ server/events/github_app_working_dir.go
@@ -2,6 +2,8 @@ package events
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 	"strings"
 
 	"github.com/mitchellh/go-homedir"
@@ -18,6 +20,10 @@ type GithubAppWorkingDir struct {
 	WorkingDir
 	Credentials    vcs.GithubCredentials
 	GithubHostname string
+}
+
+type GithubAppFileWorkspace struct {
+	FileWorkspace
 }
 
 // Clone writes a fresh token for Github App authentication
@@ -52,4 +58,86 @@ func (g *GithubAppWorkingDir) Clone(log logging.SimpleLogging, headRepo models.R
 	headRepo.SanitizedCloneURL = strings.Replace(baseRepo.SanitizedCloneURL, "://:", "://x-access-token:", 1)
 
 	return g.WorkingDir.Clone(log, headRepo, p, workspace)
+}
+
+func (g *GithubAppFileWorkspace) forceClone(log logging.SimpleLogging,
+	cloneDir string,
+	headRepo models.Repo,
+	p models.PullRequest) error {
+
+	err := os.RemoveAll(cloneDir)
+	if err != nil {
+		return errors.Wrapf(err, "deleting dir %q before cloning", cloneDir)
+	}
+
+	// Create the directory and parents if necessary.
+	log.Info("creating dir %q", cloneDir)
+	if err := os.MkdirAll(cloneDir, 0700); err != nil {
+		return errors.Wrap(err, "creating new workspace")
+	}
+
+	// During testing, we mock some of this out.
+	headCloneURL := headRepo.CloneURL
+	if g.FileWorkspace.TestingOverrideHeadCloneURL != "" {
+		headCloneURL = g.FileWorkspace.TestingOverrideHeadCloneURL
+	}
+	baseCloneURL := p.BaseRepo.CloneURL
+	if g.FileWorkspace.TestingOverrideBaseCloneURL != "" {
+		baseCloneURL = g.FileWorkspace.TestingOverrideBaseCloneURL
+	}
+
+	var cmds [][]string
+	if g.FileWorkspace.CheckoutMerge {
+		// NOTE: We can't do a shallow clone when we're merging because we'll
+		// get merge conflicts if our clone doesn't have the commits that the
+		// branch we're merging branched off at.
+		// See https://groups.google.com/forum/#!topic/git-users/v3MkuuiDJ98.
+		cmds = [][]string{
+			{
+				"git", "clone", "--branch", p.BaseBranch, "--single-branch", baseCloneURL, cloneDir,
+			},
+			{
+				"git", "remote", "add", "head", headCloneURL,
+			},
+			{
+				"git", "fetch", "head", fmt.Sprintf("pull/%s/head:", p.Num),
+			},
+			// We use --no-ff because we always want there to be a merge commit.
+			// This way, our branch will look the same regardless if the merge
+			// could be fast forwarded. This is useful later when we run
+			// git rev-parse HEAD^2 to get the head commit because it will
+			// always succeed whereas without --no-ff, if the merge was fast
+			// forwarded then git rev-parse HEAD^2 would fail.
+			{
+				"git", "merge", "-q", "--no-ff", "-m", "atlantis-merge", "FETCH_HEAD",
+			},
+		}
+	} else {
+		cmds = [][]string{
+			{
+				"git", "clone", "--branch", p.HeadBranch, "--depth=1", "--single-branch", headCloneURL, cloneDir,
+			},
+		}
+	}
+
+	for _, args := range cmds {
+		cmd := exec.Command(args[0], args[1:]...) // nolint: gosec
+		cmd.Dir = cloneDir
+		// The git merge command requires these env vars are set.
+		cmd.Env = append(os.Environ(), []string{
+			"EMAIL=atlantis@runatlantis.io",
+			"GIT_AUTHOR_NAME=atlantis",
+			"GIT_COMMITTER_NAME=atlantis",
+		}...)
+
+		cmdStr := g.FileWorkspace.sanitizeGitCredentials(strings.Join(cmd.Args, " "), p.BaseRepo, headRepo)
+		output, err := cmd.CombinedOutput()
+		sanitizedOutput := g.FileWorkspace.sanitizeGitCredentials(string(output), p.BaseRepo, headRepo)
+		if err != nil {
+			sanitizedErrMsg := g.FileWorkspace.sanitizeGitCredentials(err.Error(), p.BaseRepo, headRepo)
+			return fmt.Errorf("running %s: %s: %s", cmdStr, sanitizedOutput, sanitizedErrMsg)
+		}
+		log.Debug("ran: %s. Output: %s", cmdStr, strings.TrimSuffix(sanitizedOutput, "\n"))
+	}
+	return nil
 }
